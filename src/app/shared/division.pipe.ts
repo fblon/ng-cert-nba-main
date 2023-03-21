0e07aa3af88fb76674cde812acfa932c3615a58f
F1FB <F1FB@eurofins.com>
2023-03-21 10:28:11 +0100

    Add missing file

---
+++ src/app/shared/division.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'division'
+})
+export class DivisionPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return `${value} division`;
+  }
+
+}
