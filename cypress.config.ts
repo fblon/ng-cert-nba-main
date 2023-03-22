3ee3065dbf4f6be7f553ba78b58590d3af7d3ae5
F1FB <F1FB@eurofins.com>
2023-03-22 15:53:33 +0100

    - Make one big test to check interface as adviced in cypress doc at https://docs.cypress.io/guides/references/best-practices#Creating-tiny-tests-with-a-single-assertion - Stabilize tests re-run with increased request timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200/',
     specPattern: "cypress/tests/**/*.cy.ts",
+    requestTimeout: 200000, // xhr requests sometimes cancelled without increasing timeout :/
     setupNodeEvents(on, config) {
       if (config.isTextTerminal) {
         // skip the all.cy.js spec in "cypress run" mode
--- cypress/tests/home.cy.ts
+++ cypress/tests/home.cy.ts
@@ -5,17 +5,50 @@ describe('Home page', () => {
     cy.visit('/');
     cy.reload(true);
 
-    cy.intercept(`${apiTeams}*`, { fixture: 'teams.json' }).as('teams');
-    cy.wait('@teams');
+    cy.intercept(`${apiTeams}*`, { fixture: 'teams.json' }).as('apiTeams');
+    cy.wait('@apiTeams');
+
+    cy.get('select').eq(0).as('conferences');
+    cy.get('select').eq(1).as('divisions');
+    cy.get('select').eq(2).as('teams');
+    cy.get('select').eq(3).as('days');
+    cy.get('button').as('trackButton');
   });
 
   describe('when accessed', () => {
-    it('should display title', () => {
+    it('should display title, drop-down lists and track team button', () => {
+
       cy.get('h1').contains('NBA Score Tracking App').should('exist');
-    });
-  
-    it.only('should display teams drop-down list', () => {
-      cy.get('select').eq(2).within(() => {
+
+      cy.get('@conferences').within(() => {
+        cy.get('option').should($options => {
+            const conferences = $options.map((i, el) => Cypress.$(el).text().trim());
+
+            expect(conferences.get()).to.deep.eq([
+              '',
+              'Eastern conference',
+              'Western conference'
+            ]);
+          })
+      });
+
+      cy.get('@divisions').within(() => {
+        cy.get('option').should($options => {
+            const conferences = $options.map((i, el) => Cypress.$(el).text().trim());
+
+            expect(conferences.get()).to.deep.eq([
+              '', 
+              'Atlantic division', 
+              'Central division', 
+              'Southeast division', 
+              'Northwest division', 
+              'Pacific division', 
+              'Southwest division'
+            ]);
+          })
+      });
+
+      cy.get('@teams').within(() => {
         cy.get('option').should($options => {
             expect($options).to.have.length(30);
 
@@ -24,6 +57,17 @@ describe('Home page', () => {
             expect(firstTeamFullName).to.equal('Atlanta Hawks (Fake)');
           })
       });
+
+      cy.get('@days').within(() => {
+        cy.get('option').should($options => {
+          const conferences = $options.map((i, el) => +Cypress.$(el).text().trim());
+
+          expect(conferences.get()).to.deep.eq([6, 12, 20]);
+        })
+      });
+
+      cy.get('@trackButton').should('exist');
+
     })
   });
 })
